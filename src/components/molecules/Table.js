16a32dd56bd683bdf30825ae6081d5cd69233c0c
salyus2 <54740605+salyus2@users.noreply.github.com>
2020-07-20 19:55:19 +0200

    Mapeo Tabla Admin
    Arreglada la llamada al back y el mapeo de la info.

--- src/components/molecules/Table.js
+++ src/components/molecules/Table.js
@@ -1,67 +1,126 @@
 import React from "react";
 
+ 
+
 import getDataRows from "./../fetch/getDataRows"
+
+ 
 
 import "./../../css/molecules/table.css"
 
+ 
+
 class Table extends React.Component {
+
     constructor(props) {
+
         super(props)
+
         this.state = { rows: [] }
+
     }
 
+ 
+
     componentDidMount(prevProps) {
-        const data = getDataRows()
-        this.setState({ rows: data })
+
+    getDataRows().then(data=>{
+
+ 
+
+        for(const row of data){
+
+            let output = `
+
+            <tr>
+
+            <td>${row.workSheetId}</td>
+
+            <td>${row.workPlace}</td>
+
+            <td>${row.workDate}</td>
+
+            <td>${row.workTimeIn}</td>
+
+            <td>${row.workTimeOut}</td>
+
+            <td>${row.observations}</td>
+
+            </tr> `
+
+           document.getElementById("t_body").innerHTML= output + document.getElementById("t_body").innerHTML;
+
+        }
+
+    })
+
+        
+
     }
+
 
 
 
 
     render() {
 
+ 
+
         return (
+
             <table id="work_sheets">
+
                 <tr className="table_header">
+
+ 
 
                     <th>Numero Identificación</th>
 
+ 
+
                     {/* <th>Opciones</th> */}
+
+ 
 
                     <th>Lugar de Trabajo</th>
 
+ 
+
                     <th>Fecha</th>
+
+ 
 
                     <th>Hora Entrada</th>
 
+ 
+
                     <th>Hora Salida</th>
+
+ 
 
                     <th>Observaciones</th>
 
+ 
+
                 </tr>
 
-                {/* {this.state.rows &&
-    this.state.rows.map((row) => 
-    <React.Fragment>
-    <td>{row.workSheetId}</td>
-    <td>{row.workPlace}</td>
-    <td>{row.workDate}</td>
-    <td>{row.workTimeIn}</td>
-    <td>{row.workTimeOut}</td>
-    <td>{row.observations}</td>
-    </React.Fragment>
+                <tbody id="t_body">
 
+                </tbody>
 
-    )
-} */}
-
-
+ 
 
             </table>
+
         )
+
     }
+
 }
 
+ 
 
 export default Table
+
+ 
 
